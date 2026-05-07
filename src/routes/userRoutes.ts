ec99154e81cf7e83a89b93e9fdd79e4a3f947173
omprakash kumar <omprakashkumarglb@gmail.com>
2026-05-07 20:43:45 +0530

    remove error coming from wrong import

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,14 @@
-import express from 'express';
-import { getUsers, deleteUser } from '../controllers/userController';
-import { protect } from '../middleware/authMiddleware';
+// import express from 'express';
+// import { getUsers, deleteUser } from '../controllers/userController';
+// import { protect } from '../middleware/authMiddleware';
+// 
+// const router = express.Router();
+// 
+// router.route('/')
+//   .get(protect, getUsers);
+// 
+// router.route('/:id')
+//   .delete(protect, deleteUser);
+// 
+// export default router;
 
-const router = express.Router();
-
-router.route('/')
-  .get(protect, getUsers);
-
-router.route('/:id')
-  .delete(protect, deleteUser);
-
-export default router;
