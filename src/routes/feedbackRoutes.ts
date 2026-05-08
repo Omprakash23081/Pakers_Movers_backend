d1a8252a0e9f19007d9e182c47885576860f7fb0
omprakash kumar <omprakashkumarglb@gmail.com>
2026-05-08 08:11:48 +0530

    solve feedback routiong and controller logic

--- src/routes/feedbackRoutes.ts
+++ src/routes/feedbackRoutes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 console.log('Feedback Router Initialized');
 
 router.post('/', createFeedback);
-router.get('/', protect, getFeedbacks);
+router.get('/', getFeedbacks);
 router.delete('/:id', protect, deleteFeedback);
 
 
--- src/server.ts
+++ src/server.ts
@@ -26,6 +26,10 @@ const allowedOrigins = [
     'https://sunitacargopackersmovers.com',
     'http://localhost:3000',
     'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004',
+    'http://localhost:3005',
     'http://localhost:5173'
 ];
 
